21bdbfd795ed890faeed02b8c90de24c94accf46
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-04-14 00:35:00 +0200

    fix

---
+++ mysql2mysqli.php
@@ -0,0 +1,36 @@
+<?php
+$_connection = NULL;
+
+function mysql_connect($server, $user, $password){
+	global $_connection;
+
+	$_connection = mysqli_connect($server, $user, $password);
+}
+
+function mysql_select_db($db){
+	global $_connection;
+
+	return mysqli_select_db($_connection, $db);
+}
+
+function mysql_query($query){
+	global $_connection;
+
+	return mysqli_query($_connection, $query);
+}
+
+function mysql_fetch_array($result){
+	return mysqli_fetch_array($result);
+}
+
+function mysql_num_rows($result){
+	return mysqli_num_rows($result);
+}
+
+
+function mysql_real_escape_string($string){
+	global $_connection;
+
+	return mysqli_real_escape_string($_connection, $string);
+}
+
